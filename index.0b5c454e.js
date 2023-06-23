3e340ce1e502414b58a77babdb8ec821f6a0f266
Michael-Zhinchyn <124111477+Michael-Zhinchyn@users.noreply.github.com>
2023-06-23 00:56:17 +0000

    Deploying to gh-pages from @ Michael-Zhinchyn/goit-js-hw-11@0bb371c936dc56632d4f5a671a0c32c805452184 🚀

---
+++ index.0b5c454e.js
@@ -0,0 +1,2 @@
+document.querySelector("#search-form"),document.querySelector(".gallery"),document.querySelector(".load-more");
+//# sourceMappingURL=index.0b5c454e.js.map
---
+++ index.24a5f0b9.js
@@ -0,0 +1,2 @@
+document.querySelector("#search-form"),document.querySelector(".gallery"),document.querySelector(".load-more");
+//# sourceMappingURL=index.24a5f0b9.js.map
