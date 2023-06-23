8bfc12aa87cd660840a3cf7df449d912e9054391
Michael-Zhinchyn <124111477+Michael-Zhinchyn@users.noreply.github.com>
2023-06-22 21:07:00 -0500

    update js.
    Libraries installed, some new js additions

--- src/js/image-finder.js
+++ src/js/image-finder.js
@@ -1,3 +1,15 @@
+// Імпортую бібліотеки
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+//  Notify.Failure('Щось пішло не так'); використати в обробці помилок промісів
+
+// Оголошую глобальні змінні
 const form = document.querySelector('#search-form');
 const gallery = document.querySelector('.gallery');
 const loadMoreBtn = document.querySelector('.load-more');
+const BASE_URL = 'https://pixabay.com/api/';
+const API_KEY = '37718597-f2a776258a6c278a1ed771723';
+let page = 1;
+
+// form.addEventListener('submit', e => {
+//   e.preventDefault();
+// });
