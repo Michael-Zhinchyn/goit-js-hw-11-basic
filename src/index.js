9ec41d8bc581abe8aff280afb3b67167432a1063
Michael-Zhinchyn <124111477+Michael-Zhinchyn@users.noreply.github.com>
2023-06-29 16:57:07 -0500

    update

--- src/index.js
+++ src/index.js
@@ -1,53 +1,78 @@
+// Підключаємо різні модулі та файлі для роботи з анімацією, lightbox та API
 import './js/animation';
-
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-
 import {
   getImages,
   setSearchQuery,
   resetPage,
   nextPage,
-} from './js/apiService'; // import resetPage and nextPage
+} from './js/apiService';
 import { createMarkup } from './js/markup';
 import { Notify } from 'notiflix';
 
+// Отримуємо доступ до елементів DOM
 const form = document.querySelector('#search-form');
 const gallery = document.querySelector('.gallery');
 const loadMoreBtn = document.querySelector('.load-more');
+
+// Спочатку приховуємо кнопку "Load more"
 loadMoreBtn.style.display = 'none';
 
+// Додаємо обробник подій для форми
 form.addEventListener('submit', onSubmit);
 
+// Функція, яка запускається при відправці форми
 function onSubmit(evt) {
   evt.preventDefault();
 
+  // Перевіряємо, чи поле вводу не порожнє
   if (evt.target.searchQuery.value === '') {
     Notify.warning('Input field is empty');
     return;
   }
+
+  // Задаємо запит для пошуку
   setSearchQuery(evt.target.searchQuery.value);
+
+  // Скидаємо номер сторінки до 1
   resetPage();
-  loadMoreBtn.hidden = true; // hide button before getting images
+
+  // Приховуємо кнопку "Load more" поки не отримаємо результати
+  loadMoreBtn.hidden = true;
+
+  // Отримуємо зображення з API
   getImages().then(data => {
+    // Вставляємо отримані зображення в галерею
     gallery.insertAdjacentHTML('beforeend', createMarkup(data));
+
+    // Ініціалізуємо lightbox
     new SimpleLightbox('.gallery a', {
       captionDelay: 200,
       captionsData: 'alt',
     });
-    loadMoreBtn.hidden = false; // show button after images were loaded
+
+    // Показуємо кнопку "Load more"
+    loadMoreBtn.hidden = false;
     loadMoreBtn.style.display = 'block';
   });
 }
 
+// Додаємо обробник подій для кнопки "Load more"
 loadMoreBtn.addEventListener('click', onLoadMore);
 
+// Функція, яка запускається при натисканні кнопки "Load more"
 function onLoadMore() {
-  nextPage().then(renderGallery); // load next page on button click
+  // Завантажуємо наступну сторінку з результатами
+  nextPage().then(renderGallery);
 }
 
+// Функція для додавання нових зображень в галерею
 function renderGallery(data) {
+  // Вставляємо отримані зображення в галерею
   gallery.insertAdjacentHTML('beforeend', createMarkup(data));
+
+  // Ініціалізуємо lightbox для нових зображень
   new SimpleLightbox('.gallery a', {
     captionDelay: 200,
     captionsData: 'alt',
--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,21 +1,29 @@
+// Імпортуємо необхідні бібліотеки
 import axios from 'axios';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
+// Оголошуємо базові константи для API
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '37718597-f2a776258a6c278a1ed771723';
+
+// Ініціалізуємо змінні стану
 let searchQuery = '';
 let currentPage = 1;
 
+// Функція для переходу до наступної сторінки
 export function nextPage() {
   currentPage += 1;
   return getImages();
 }
 
+// Функція для скидання номера сторінки до 1
 export function resetPage() {
   currentPage = 1;
 }
 
+// Асинхронна функція для виконання API-запиту і отримання зображень
 export async function getImages() {
+  // Створюємо параметри запиту
   const params = new URLSearchParams({
     key: `${API_KEY}`,
     q: `${searchQuery}`,
@@ -26,8 +34,11 @@ export async function getImages() {
     per_page: 40,
   });
 
+  // Виконуємо запит
   try {
     const response = await axios.get(`${BASE_URL}?${params}`);
+
+    // Перетворюємо відповідь в бажаний формат
     let imagesData = response.data.hits.map(hit => ({
       webformatURL: hit.webformatURL.replace('_640', '_340'),
       largeImageURL: hit.largeImageURL,
@@ -38,12 +49,15 @@ export async function getImages() {
       likes: hit.likes,
     }));
 
+    // Повертаємо дані
     return imagesData;
   } catch (error) {
+    // Якщо виникає помилка, показуємо повідомлення
     Notify.failure(error.message);
   }
 }
 
+// Функція для оновлення пошукового запиту
 export function setSearchQuery(newQuery) {
   searchQuery = newQuery;
 }
