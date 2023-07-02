cd8da7dc5b4661ab0c843a0083b9d21dc6a6aaf9
Michael-Zhinchyn <124111477+Michael-Zhinchyn@users.noreply.github.com>
2023-07-02 18:24:01 -0500

    update

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 // Підключаємо різні модулі та файлі для роботи з анімацією, lightbox та API
-import './js/animation';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import {
